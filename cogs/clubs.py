fc1d3d895aa8f84f75b84a239e32c10900588fed
Bill Christian Tyros <tyros.bill@gmail.com>
2018-09-27 21:51:48 -0400

    Add add+remove club member functions and improve existing club create and delete functions

--- cogs/clubs.py
+++ cogs/clubs.py
@@ -22,10 +22,9 @@ class Clubs():
         if ctx.invoked_subcommand is None:
             raise commands.MissingRequiredArgument()
 
-    @_club.command(name='create', aliases=['c', 'cr', 'cre', 'creat'], pass_context=True)
+    @_club.command(name='create_club', aliases=['c', 'cr', 'cre', 'creat', 'create', 'create_', 'create_c', 'create_cl', 'create_clu'], pass_context=True)
     async def _create(self, ctx, club_name : str, role_name : str, *members : discord.Member):
-        """Creates a club (a private channel under the club category) viewable
-        only by people with the given role."""
+        """Create a club channel (a private channel) for people with a specific role."""
         if len(club_name) <= 3:
             raise commands.BadArgument('Club channel name given is too short.')
         elif len(role_name) <= 3:
@@ -37,31 +36,46 @@ class Clubs():
         elif len(members) < 2:
             raise commands.BadArgument('You must create a club with at least two members.')
         author = ctx.message.author
-        clubRole = await self.bot.create_role(author.server, name=role_name)
+        club_role = await self.bot.create_role(author.server, name=role_name)
         for member in members:
-            await self.bot.add_roles(member, clubRole)
-        everyonePermissions = discord.PermissionOverwrite(read_messages=False)
-        clubMembersPermissions = discord.PermissionOverwrite(read_messages=True, send_messages=True)
-        clubChannel = await self.bot.create_channel(author.server, club_name, (author.server.default_role, everyonePermissions), (clubRole, clubMembersPermissions))
-        await self.bot.say("{} added a channel '{}' with role '{}'".format(
-            ctx.message.author.mention,
-            clubChannel,
-            clubRole)
-        )
+            await self.bot.add_roles(member, club_role)
+        everyone_permissions = discord.PermissionOverwrite(read_messages=False)
+        club_members_permissions = discord.PermissionOverwrite(read_messages=True, send_messages=True)
+        club_channel = await self.bot.create_channel(author.server, club_name, (author.server.default_role, everyone_permissions), (club_role, club_members_permissions))
 
-    @_club.command(name='delete', aliases=['d', 'de', 'del', 'dele', 'delet'], pass_context=True)
+    @_club.command(name='add_member', aliases=['a', 'ad', 'add', 'add_', 'add_m', 'add_me', 'add_mem', 'add_memb', 'add_membe'], pass_context=True)
+    async def _add(self, ctx, club : discord.Channel, member : discord.Member):
+        """Add a new member to a club channel."""
+        club_role = self.find_club_role(club)
+        if club_role in member.roles:
+            raise commands.BadArgument("Given member already belongs to the given club.")
+        await self.bot.add_roles(member, club_role)
+
+    @_club.command(name='remove_member', aliases=['r', 're', 'rem', 'remo', 'remov', 'remove', 'remove_', 'remove_m', 'remove_me', 'remove_mem', 'remove_memb', 'remove_membe'], pass_context=True)
     @checks.is_owner()
-    async def _delete(self, ctx, club : discord.Channel, role : discord.Role):
-        """[ADMIN] Deletes a club only by people with the given role."""
-        # TODO(keikakub): check if role exists and is tied to the given channel, also test if channel exists
-        await self.bot.say("{} is deleting channel '{}' and role '{}'".format(
-            ctx.message.author.mention,
-            club,
-            role)
-        )
-        await self.bot.delete_role(ctx.message.author.server, role)
+    async def _remove(self, ctx, club : discord.Channel, member : discord.Member):
+        """[ADMIN] Remove a member from a club channel."""
+        club_role = self.find_club_role(club)
+        if club_role not in member.roles:
+            raise commands.BadArgument("Given member does not belong to the given club.")
+        await self.bot.remove_roles(member, club_role)
+
+    @_club.command(name='delete_club', aliases=['d', 'de', 'del', 'dele', 'delet', 'delete', 'delete_', 'delete_c', 'delete_cl', 'delete_clu'], pass_context=True)
+    @checks.is_owner()
+    async def _delete(self, ctx, club : discord.Channel):
+        """[ADMIN] Delete a club channel and its associated role."""
+        club_role = self.find_club_role(club)
+        await self.bot.delete_role(ctx.message.author.server, club_role)
         await self.bot.delete_channel(club)
-        await self.bot.say("Done... ")
+
+    def find_club_role(self, club : discord.Channel):
+        if len(club.changed_roles) != 2:
+            raise commands.BadArgument("Given club channel doesn't look like a club.")
+        for role in club.changed_roles:
+            if not role.is_everyone:
+                return role
+        raise commands.BadArgument("No role found for this club.")
+
 
 def setup(bot):
     bot.add_cog(Clubs(bot))
